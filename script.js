d0ca0ee32bc00a98b5306dd939db7cc2cfbd9d78
Caitlin Swickard <caitlin6029@gmail.com>
2021-11-17 17:58:13 -0700

    completed line fill logic for progress bar

--- script.js
+++ script.js
@@ -13,6 +13,42 @@ next.addEventListener('click', () => {
   if (currentActive > circles.length) {
     currentActive = circles.length;
   }
-  console.log(currentActive);
+  // console.log(currentActive);
+  update();
+});
 
+prev.addEventListener('click', () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
 })
+
+
+// function to update the active class on the progress bar
+const update = () => {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  })
+
+  // logic to fill out the blue line as progress completes with next button
+  const actives = document.querySelectorAll('.active');
+  // console.log((actives.length / circles.length) * 100);
+  progress.style.width = (actives.length - 1) / (circles.length - 1) * 99 + "%";
+
+  // disable buttons logic
+  if (currentActive === 1) {
+    prev.disabled = true
+  } else if (currentActive === circles.length) {
+    next.disabled = true
+  } else {
+    prev.disabled = false
+    next.disabled = false
+  }
+}
